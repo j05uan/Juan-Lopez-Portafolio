93822033f1c08db79e972560147b401e9d1e9ac0
j05uan <lopezamayajuanpablo@gmail.com>
2024-08-20 17:32:27 -0500

    creaccion de la pagina de inicio

--- apps/index.js
+++ apps/index.js
@@ -57,3 +57,45 @@ $(document).ready(function (){
         $("#myCanvasContainer");
     }
 })
+function flipPage() {
+    const pagina1 = document.getElementById('pagina1');
+    const pagina2 = document.getElementById('pagina2');
+    const derechaInicio = document.getElementById('derechaInicio');
+
+    pagina1.classList.add('flip');
+    derechaInicio.classList.add('flip2');
+
+    setTimeout(() => {
+        pagina1.style.zIndex = '1';
+        pagina2.style.zIndex = '2';
+        pagina2.style.backfaceVisibility = 'visible';
+        pagina1.style.backfaceVisibility = 'hidden';
+
+        pagina2.classList.add('show');
+    }, 1000); // Tiempo debe coincidir con la animación de flip
+}
+
+function start() {
+    const logo = document.getElementById('logo');
+    const circulo1 = document.getElementById('circulo1');
+    const circulo2 = document.getElementById('circulo2');
+    const circulo3 = document.getElementById('circulo3');
+
+    // Añadir animaciones
+    logo.classList.add('rotate');
+    circulo1.classList.add('righrotate');
+    circulo1.style.borderLeft='5px solid #fff';
+    circulo2.classList.add('leftrotate');
+    circulo2.style.borderRight='5px solid #fff';
+    circulo3.classList.add('righrotate');
+    circulo3.style.borderTop='3px solid #fff';
+
+    // Después de 1 segundo (coincidiendo con la animación de rotación del logo) iniciar el cambio de página
+    setTimeout(() => {
+        flipPage();
+    }, 1000); // Tiempo debe coincidir con la animación de rotate
+}
+
+// Añadir un listener para el clic en cualquier parte de la página
+document.addEventListener('click', start);
+
