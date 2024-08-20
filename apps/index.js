87cd965665ed245ea1da233c2b4b2df552679d54
Juan Pablo Lopez Amaya <152333487+j05uan@users.noreply.github.com>
2024-08-20 13:17:32 -0500

    Update index.js

--- apps/index.js
+++ apps/index.js
@@ -44,3 +44,16 @@ const myFunc = (navCondition) => {
     }
 }
 
+$(document).ready(function (){
+    if(!$("#myCanvas").tagcanvas({
+        textColour: "#08fdd8",
+        outlineColour: "transparent",
+        reverse: true,
+        depth: 0.8,
+        maxSpeed: 0.05,
+        weight: true,
+    }, "tags")){
+        // something went wrong hide the canvas container,
+        $("#myCanvasContainer");
+    }
+})
